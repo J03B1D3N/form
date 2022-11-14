5c54febb4cd25453cdf4a7d8001e2b404e4d926a
Justas Lapiene <justas63@gmail.com>
2022-11-14 20:17:41 +0100

    password match check now works as intended

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const submit = document.querySelector(".submit")
+const submit = document.getElementById("submit")
+const password = document.getElementById("password")
+const confirmPass = document.getElementById("passConfirm")
+const form = document.getElementById("form")
+const passMatch = document.getElementById("passMatchCheck")
+
+
+
+
+
+const check = function() {
+        if (password.value == confirmPass.value) {
+          confirmPass.setCustomValidity("")
+          passMatch.innerHTML = "Passwords match"
+        } else {
+            confirmPass.setCustomValidity("Invalid field")
+            passMatch.innerHTML = "Passwords don't match"
+        }
+      }
+
